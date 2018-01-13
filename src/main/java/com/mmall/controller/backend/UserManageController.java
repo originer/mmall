5abc2fcf22819ee26e9c88acd91f8fe33a05c57e
Zz <997072815@qq.com>
2018-01-13 19:55:16 +0800

    bug fix

--- src/main/java/com/mmall/controller/backend/UserManageController.java
+++ src/main/java/com/mmall/controller/backend/UserManageController.java
@@ -4,6 +4,10 @@ import com.mmall.common.Const;
 import com.mmall.common.ServerResponse;
 import com.mmall.pojo.User;
 import com.mmall.service.IUserService;
+import com.mmall.util.CookieUtil;
+import com.mmall.util.JsonUtil;
+import com.mmall.util.RedisPoolUtil;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -11,6 +15,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 /**
@@ -32,7 +37,15 @@ public class UserManageController {
             User user = response.getData();
             if (user.getRole() == Const.Role.ROLE_ADMIN) {
                 //说明登录的是管理员
-                session.setAttribute(Const.CURRENT_USER, user);
+                String loginToken = CookieUtil.readLoginToken(httpServletRequest);
+                if (StringUtils.isEmpty(loginToken)) {
+                    return ServerResponse.createByErrorMessage("用户未登录,无法获取当前用户的信息");
+                }
+                String userJson = RedisPoolUtil.get(loginToken);
+                user = JsonUtil.string2Obj(userJson, User.class);
+                if (user == null) {
+                    return ServerResponse.createByErrorMessage("用户未登录");
+                }
                 return response;
             } else {
                 return ServerResponse.createByErrorMessage("不是管理员,无法登录");
