253dd8951c4f45f354be9822ee690415be0dc1d0
Zz <997072815@qq.com>
2018-01-21 01:06:51 +0800

    利用Redis分布式锁实现定时关单功能

--- src/main/java/com/mmall/task/CloseOrderTask.java
+++ src/main/java/com/mmall/task/CloseOrderTask.java
@@ -6,7 +6,11 @@ import com.mmall.util.PropertiesUtil;
 import com.mmall.util.RedisShardedPoolUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import javax.annotation.PreDestroy;
+
 /**
  * @author Zz
  **/
@@ -16,6 +20,11 @@ public class CloseOrderTask {
     @Autowired
     private IOrderService iOrderService;
 
+    //在调用tomcat shutdown方法时会自动调用
+    @PreDestroy
+    public void delLock() {
+        RedisShardedPoolUtil.del(Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK);
+    }
 //    @Scheduled(cron="0 */1 * * * ?") //每分钟的整数倍执行
     public void closeOrderTaskV1() {
         log.info("关闭订单定时任务启动");
@@ -24,14 +33,19 @@ public class CloseOrderTask {
         log.info("关闭订单定时任务结束");
     }
 
+    /**
+     * 这种方案，在tomcat突然被kill掉时还是有可能发生死锁
+     */
+//    @Scheduled(cron="0 */1 * * * ?")
     public void closeOrderTaskV2() {
         log.info("关闭订单定时任务启动");
 
         long lockTimeout = Long.parseLong(PropertiesUtil.getProperty("lock.timeout", "50000"));
-
+        //如果执行到这里kill tomcat closeOrder中的expire不会被调用，就产生了死锁
         Long setnxResult = RedisShardedPoolUtil.setnx(Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK, String.valueOf(System.currentTimeMillis() + lockTimeout));
         if (setnxResult != null && setnxResult.intValue() == 1) {
             //如果返回是1，代表设置成功，获取锁
+            closeOrder(Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK);
 
         } else {
             log.info("没有获得分布式锁：{}", Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK);
@@ -39,6 +53,42 @@ public class CloseOrderTask {
 
         log.info("关闭订单定时任务结束");
     }
+
+    /**
+     * 双重分布式锁方案
+     */
+    @Scheduled(cron="0 */1 * * * ?")
+    public void closeOrderTaskV3() {
+        log.info("关闭订单定时任务启动");
+        long lockTimeout = Long.parseLong(PropertiesUtil.getProperty("lock.timeout", "50000"));
+        //如果执行到这里kill tomcat closeOrder中的expire不会被调用，就产生了死锁
+        Long setnxResult = RedisShardedPoolUtil.setnx(Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK, String.valueOf(System.currentTimeMillis() + lockTimeout));
+        if (setnxResult != null && setnxResult.intValue() == 1) {
+            //如果返回是1，代表设置成功，获取锁
+            closeOrder(Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK);
+        } else {
+            //未获取到锁，继续判断，判断时间戳，看是否可以重置并获取到锁
+            String lockValueStr = RedisShardedPoolUtil.get(Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK);
+            if (lockValueStr != null && System.currentTimeMillis() > Long.parseLong(lockValueStr)) {
+                //到这里说明可以重置锁，执行getSet原子操作，tomcat集群下lockValueStr可能不等于getSetResult
+                String getSetResult = RedisShardedPoolUtil.getSet(Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK, String.valueOf(System.currentTimeMillis() + lockTimeout));
+                //返回给定key的旧值，判断是否可以获取锁
+                //当key没有旧值时，即key不存在时，返回nil ->获取锁
+                //有旧值时，set一个新的value，获取旧的值，如果value没被改变 可以获取锁
+
+                if (getSetResult == null || (getSetResult != null && getSetResult.equals(lockValueStr))) {
+                    closeOrder(Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK);
+                } else {
+                    log.info("没有获得分布式锁：{}", Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK);
+                }
+            } else {
+                log.info("没有获得分布式锁：{}", Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK);
+            }
+        }
+
+        log.info("关闭订单定时任务结束");
+    }
+
 
     private void closeOrder(String lockName) {
         RedisShardedPoolUtil.expire(lockName,50); //设置有效期50秒，防止死锁
--- src/main/java/com/mmall/util/RedisShardedPoolUtil.java
+++ src/main/java/com/mmall/util/RedisShardedPoolUtil.java
@@ -104,7 +104,24 @@ public class RedisShardedPoolUtil {
             jedis = RedisShardedPool.getJedis();
             result = jedis.setnx(key, value);
         } catch (Exception e) {
-            log.error("set key:{} value:{} error", key, value, e);
+            log.error("setnx key:{} value:{} error", key, value, e);
+            RedisShardedPool.returnBrokeResource(jedis);
+            return result;
+        }
+        RedisShardedPool.returnResource(jedis);
+        return result;
+    }
+
+
+    public static String getSet(String key, String value) {
+        ShardedJedis jedis = null;
+        String result = null;
+
+        try {
+            jedis = RedisShardedPool.getJedis();
+            result = jedis.getSet(key, value);
+        } catch (Exception e) {
+            log.error("getset key:{} value:{} error", key, value, e);
             RedisShardedPool.returnBrokeResource(jedis);
             return result;
         }
